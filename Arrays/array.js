ddd21fb2e6994c0f26160e1a58219faeb05e0301
Febin Bellamy <febinbellamy@gmail.com>
2022-08-18 07:28:49 -0400

    Arrays

---
+++ Arrays/array.js
@@ -0,0 +1,19 @@
+let anime = ['naruto', 'deathNote', 'bleach', 'onePiece', 'attackOnTitan', 'scissorSeven', 'hunterXHunter', 'spyXFamily'];
+
+console.log(anime);
+
+let oneofBestShows = anime[1];
+
+console.log(oneofBestShows);
+
+// Category: anime 
+
+// List Items:
+// naruto
+// deathNote
+// bleach
+// onePiece
+// attackOnTitan
+// scissorSeven
+// hunterXHunter
+// spyXFamily
---
+++ Arrays/random.js
@@ -0,0 +1,10 @@
+let randDecimal = Math.random();
+console.log(randDecimal);
+
+let randNum = randDecimal * 10;
+console.log(randNum);
+
+let randInt = Math.floor(randNum);
+console.log(randInt);
+
+
