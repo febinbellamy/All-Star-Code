51c3a96af78b8332f0345b077ee348dc5b30c650
Febin Bellamy <febinbellamy@gmail.com>
2022-08-22 11:37:24 -0400

    Monty hall

---
+++ montyHallGame/script.js
@@ -0,0 +1,6 @@
+// 3 possible scenarios: the car is behind the first door, the second door, or the third door
+let sets = [
+  ["imgs/car.png", "imgs/goat.png", "imgs/goat.png"],
+  ["imgs/goat.png", "imgs/car.png", "imgs/goat.png"],
+  ["imgs/goat.png", "imgs/goat.png", "imgs/car.png"],
+];
